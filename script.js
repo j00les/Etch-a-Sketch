7b05db6ea1a3ebed272b0fcc5fbef1948410ee0f
Nabiel Alif <nabiel.alif01@gmail.com>
2022-03-26 14:45:17 +0700

    Add changeSize function to change grid sizew

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
-
 const gridMaker = (size) => {
     const board = document.querySelector('.board');
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < 256; i++) {
-        let grid = document.createElement('div')
-        grid.style.backgroundColor = 'blue'
-        board.appendChild(grid)
+        let grid = document.createElement('div');
+        grid.style.backgroundColor = 'blue';
+        board.appendChild(grid);
     }
-}
+};
 
+
+
+const changeSize = (input) => {
+    gridMaker(input)
+}
