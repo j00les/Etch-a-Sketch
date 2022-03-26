8951472c77d1a6218ab62b8b06ad90ee976ea38b
Nabiel Alif <nabiel.alif01@gmail.com>
2022-03-26 20:32:15 +0700

    Reset button and its function

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function gridMaker(size) {
         grid = document.createElement('div')
         grid.addEventListener('mouseover', initialGridColor)
         grid.style.cssText =
-        'background-color:white; border: 0.2px solid';
+        'background-color:white; border: 0.2px solid lightgray';
         board.appendChild(grid);
     }
 };
@@ -34,15 +34,31 @@ function changeSize(input) {
 }
 
 
-// Function to change the grid color 
+// Function to change the grid color -----------------------------
 let color = 'white';
+
 function initialGridColor() {
-    this.style.backgroundColor = color;
+    
+    if (color === 'random') {
+        this.style.background = `hsl(${Math.random() * 360}, 50%, 80%)`
+    } else {
+        this.style.backgroundColor = color;
+        
+    }
     
 }
-console.log(color)
 
 function changeColor(choice) {
     color = choice
-}
 
+}
+//  ---------------------------------------------
+
+
+function resetBoard() {
+    const board = document.querySelector('.board');
+    const clearGrid = board.querySelectorAll('div')
+    clearGrid.forEach((div) => div.style.backgroundColor = 'white');
+
+
+}
