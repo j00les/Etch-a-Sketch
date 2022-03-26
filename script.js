10af2fa85aba44c313841d3916dc6e706619ca59
Nabiel Alif <nabiel.alif01@gmail.com>
2022-03-26 19:56:17 +0700

    Buttons to change grid color

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
 
-const gridMaker = (size) => {
+let grid;
+
+
+function gridMaker(size) {
+    // The grid maker
     const board = document.querySelector('.board');
-   
-    // Clear out any existing square that are on the board 
-    let boardGrids = board.querySelectorAll('div');
-    boardGrids.forEach((div) => div.remove());
-   
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
+    
+    // Clear out any existing square that are on the board 
+    const clearGrid = board.querySelectorAll('div');
+    clearGrid.forEach((div) => div.remove());
+    
+    // Loops depends on the size input
     for (let i = 0; i < size * size; i++) {
-        let grid = document.createElement('div');
-        
-        // Grid color
-        grid.addEventListener('mouseover', () => {
-            grid.style.backgroundColor = 'blue'
-        });
-        
-        grid.style.backgroundColor = 'black';
+        grid = document.createElement('div')
+        grid.addEventListener('mouseover', initialGridColor)
+        grid.style.cssText =
+        'background-color:white; border: 0.2px solid';
         board.appendChild(grid);
     }
 };
 
 
-
-const changeSize = (input) => {
-    if (input <= 64 ) {
+// User input 
+function changeSize(input) {
+    
+    if (input <= 64) {
         gridMaker(input)
     } else {
         console.log('Too many squares')
     }
+}
+
+
+// Function to change the grid color 
+let color = 'white';
+function initialGridColor() {
+    this.style.backgroundColor = color;
+    
+}
+console.log(color)
+
+function changeColor(choice) {
+    color = choice
 }
 
