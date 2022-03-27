8cdb18cb63309eb00c34afb7ff6d7e561cb98dd7
Nabiel Alif <nabiel.alif01@gmail.com>
2022-03-27 11:13:05 +0700

    HTML semantic correction

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 
 let grid;
 
+
 function gridMaker(size) {
     // The grid maker
     const board = document.querySelector('.board');
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    
+
     // Clear out any existing square that are on the board 
     const clearGrid = board.querySelectorAll('div');
     clearGrid.forEach((div) => div.remove());
-    
+
     // Loops depends on the size input
     for (let i = 0; i < size * size; i++) {
         grid = document.createElement('div')
         grid.addEventListener('mouseover', initialGridColor)
         grid.style.cssText =
-        'background-color:white; border: 0.2px solid lightgray';
+            'background-color:white; border: 0.2px solid lightgray';
         board.appendChild(grid);
     }
 };
 
 
-// User input 
+
+
+
+// User input
 function changeSize(input) {
-    
+
     if (input <= 64) {
         gridMaker(input)
     } else {
@@ -33,7 +37,10 @@ function changeSize(input) {
 }
 
 
-// Function to change the grid color -----------------------------
+
+
+
+// Change grid color 
 let color = 'white';
 
 function initialGridColor() {
@@ -42,41 +49,41 @@ function initialGridColor() {
             this.style.background = `hsl(${Math.random() * 360}, 50%, 80%)`
         } else {
             this.style.backgroundColor = color;
-            
+
         }
     }
-    
+
 }
 
 function changeColor(choice) {
     color = choice
 }
 
-//  ---------------------------------------------
 
-
+// Reset the board 
 function resetBoard() {
     const board = document.querySelector('.board');
     const clearGrid = board.querySelectorAll('div')
     clearGrid.forEach((div) => div.style.backgroundColor = 'white');
 
-    
+
 }
 
-// Brush mode active or inactive ----------------
 
+
+
+
+// Brush mode  
 let boardClick = true
 
 document.querySelector(".board").addEventListener('click', (e) => {
-    if(e.target.tagName != 'button'){
+    if (e.target.tagName != 'button') {
         boardClick = !boardClick
         if (boardClick == true) {
             document.querySelector('.mode').textContent = 'Active'
         } else {
-            document.querySelector('.mode').textContent = 'Deactive'
+            document.querySelector('.mode').textContent = 'Inactive'
         }
     }
-    
-})
 
-// -----------------------------------
+})
